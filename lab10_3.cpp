ba501754de60789c7a85681d78f8e04986c1609f
NpS <preeyanan.nps@gmail.com>
2023-12-19 16:12:18 +0700

    10.3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    double sum, sumoqr, mean, std;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source, textline))
+    {
+        sum += atof(textline.c_str());
+        sumoqr += pow(atof(textline.c_str()), 2);
+        count++;
+    }
+    mean = sum / count;
+    std = sqrt(sumoqr / count - pow(mean, 2));
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << std;
+}
